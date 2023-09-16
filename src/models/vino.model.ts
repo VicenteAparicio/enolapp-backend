f4e3e52bdff941e25a993343a66614b276577aef
VicenteAparicio <miradadesigual@gmail.com>
2023-09-16 13:07:32 +0200

    Db structure

---
+++ src/models/vino.model.ts
@@ -0,0 +1,10 @@
+export interface IVino {
+    año: string,
+    variedad: string,
+    tipo: string,
+    color: string,
+    temperatura: number,
+    graduacion: number,
+    ph: number,
+    observaciones: string
+}
